62638a685883e83eb7e59a03545e264e84266ee5
jaewon <jaewonmichael@gmail.com>
2023-05-18 12:17:38 -0400

    #12: add theme data

---
+++ lib/theme/text_themes.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class NotedTextThemes {
+  final TextTheme poppinsTextTheme = const TextTheme(
+    displayLarge: TextStyle(fontFamily: 'Poppins', fontSize: 57, height: 64 / 57, fontWeight: FontWeight.normal),
+    displayMedium: TextStyle(fontFamily: 'Poppins', fontSize: 45, height: 52 / 45, fontWeight: FontWeight.normal),
+    displaySmall: TextStyle(fontFamily: 'Poppins', fontSize: 36, height: 44 / 36, fontWeight: FontWeight.normal),
+    headlineLarge: TextStyle(fontFamily: 'Poppins', fontSize: 32, height: 40 / 32, fontWeight: FontWeight.normal),
+    headlineMedium: TextStyle(fontFamily: 'Poppins', fontSize: 28, height: 36 / 28, fontWeight: FontWeight.normal),
+    headlineSmall: TextStyle(fontFamily: 'Poppins', fontSize: 24, height: 32 / 24, fontWeight: FontWeight.normal),
+    titleLarge: TextStyle(fontFamily: 'Poppins', fontSize: 22, height: 28 / 22, fontWeight: FontWeight.w500),
+    titleMedium: TextStyle(fontFamily: 'Poppins', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.w500),
+    titleSmall: TextStyle(fontFamily: 'Poppins', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.w500),
+    labelLarge: TextStyle(fontFamily: 'Poppins', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.normal),
+    labelMedium: TextStyle(fontFamily: 'Poppins', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+    labelSmall: TextStyle(fontFamily: 'Poppins', fontSize: 11, height: 16 / 11, fontWeight: FontWeight.normal),
+    bodyLarge: TextStyle(fontFamily: 'Poppins', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.normal),
+    bodyMedium: TextStyle(fontFamily: 'Poppins', fontSize: 16, height: 20 / 16, fontWeight: FontWeight.normal),
+    bodySmall: TextStyle(fontFamily: 'Poppins', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+  );
+
+  final TextTheme robotoTextTheme = const TextTheme(
+    displayLarge: TextStyle(fontFamily: 'Roboto', fontSize: 57, height: 64 / 57, fontWeight: FontWeight.normal),
+    displayMedium: TextStyle(fontFamily: 'Roboto', fontSize: 45, height: 52 / 45, fontWeight: FontWeight.normal),
+    displaySmall: TextStyle(fontFamily: 'Roboto', fontSize: 36, height: 44 / 36, fontWeight: FontWeight.normal),
+    headlineLarge: TextStyle(fontFamily: 'Roboto', fontSize: 32, height: 40 / 32, fontWeight: FontWeight.normal),
+    headlineMedium: TextStyle(fontFamily: 'Roboto', fontSize: 28, height: 36 / 28, fontWeight: FontWeight.normal),
+    headlineSmall: TextStyle(fontFamily: 'Roboto', fontSize: 24, height: 32 / 24, fontWeight: FontWeight.normal),
+    titleLarge: TextStyle(fontFamily: 'Roboto', fontSize: 22, height: 28 / 22, fontWeight: FontWeight.w500),
+    titleMedium: TextStyle(fontFamily: 'Roboto', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.w500),
+    titleSmall: TextStyle(fontFamily: 'Roboto', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.w500),
+    labelLarge: TextStyle(fontFamily: 'Roboto', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.normal),
+    labelMedium: TextStyle(fontFamily: 'Roboto', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+    labelSmall: TextStyle(fontFamily: 'Roboto', fontSize: 11, height: 16 / 11, fontWeight: FontWeight.normal),
+    bodyLarge: TextStyle(fontFamily: 'Roboto', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.normal),
+    bodyMedium: TextStyle(fontFamily: 'Roboto', fontSize: 16, height: 20 / 16, fontWeight: FontWeight.normal),
+    bodySmall: TextStyle(fontFamily: 'Roboto', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+  );
+
+  final TextTheme loraTextTheme = const TextTheme(
+    displayLarge: TextStyle(fontFamily: 'Lora', fontSize: 57, height: 64 / 57, fontWeight: FontWeight.normal),
+    displayMedium: TextStyle(fontFamily: 'Lora', fontSize: 45, height: 52 / 45, fontWeight: FontWeight.normal),
+    displaySmall: TextStyle(fontFamily: 'Lora', fontSize: 36, height: 44 / 36, fontWeight: FontWeight.normal),
+    headlineLarge: TextStyle(fontFamily: 'Lora', fontSize: 32, height: 40 / 32, fontWeight: FontWeight.normal),
+    headlineMedium: TextStyle(fontFamily: 'Lora', fontSize: 28, height: 36 / 28, fontWeight: FontWeight.normal),
+    headlineSmall: TextStyle(fontFamily: 'Lora', fontSize: 24, height: 32 / 24, fontWeight: FontWeight.normal),
+    titleLarge: TextStyle(fontFamily: 'Lora', fontSize: 22, height: 28 / 22, fontWeight: FontWeight.w500),
+    titleMedium: TextStyle(fontFamily: 'Lora', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.w500),
+    titleSmall: TextStyle(fontFamily: 'Lora', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.w500),
+    labelLarge: TextStyle(fontFamily: 'Lora', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.normal),
+    labelMedium: TextStyle(fontFamily: 'Lora', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+    labelSmall: TextStyle(fontFamily: 'Lora', fontSize: 11, height: 16 / 11, fontWeight: FontWeight.normal),
+    bodyLarge: TextStyle(fontFamily: 'Lora', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.normal),
+    bodyMedium: TextStyle(fontFamily: 'Lora', fontSize: 16, height: 20 / 16, fontWeight: FontWeight.normal),
+    bodySmall: TextStyle(fontFamily: 'Lora', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+  );
+
+  final TextTheme vollkornTextTheme = const TextTheme(
+    displayLarge: TextStyle(fontFamily: 'Vollkorn', fontSize: 57, height: 64 / 57, fontWeight: FontWeight.normal),
+    displayMedium: TextStyle(fontFamily: 'Vollkorn', fontSize: 45, height: 52 / 45, fontWeight: FontWeight.normal),
+    displaySmall: TextStyle(fontFamily: 'Vollkorn', fontSize: 36, height: 44 / 36, fontWeight: FontWeight.normal),
+    headlineLarge: TextStyle(fontFamily: 'Vollkorn', fontSize: 32, height: 40 / 32, fontWeight: FontWeight.normal),
+    headlineMedium: TextStyle(fontFamily: 'Vollkorn', fontSize: 28, height: 36 / 28, fontWeight: FontWeight.normal),
+    headlineSmall: TextStyle(fontFamily: 'Vollkorn', fontSize: 24, height: 32 / 24, fontWeight: FontWeight.normal),
+    titleLarge: TextStyle(fontFamily: 'Vollkorn', fontSize: 22, height: 28 / 22, fontWeight: FontWeight.w500),
+    titleMedium: TextStyle(fontFamily: 'Vollkorn', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.w500),
+    titleSmall: TextStyle(fontFamily: 'Vollkorn', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.w500),
+    labelLarge: TextStyle(fontFamily: 'Vollkorn', fontSize: 14, height: 20 / 14, fontWeight: FontWeight.normal),
+    labelMedium: TextStyle(fontFamily: 'Vollkorn', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+    labelSmall: TextStyle(fontFamily: 'Vollkorn', fontSize: 11, height: 16 / 11, fontWeight: FontWeight.normal),
+    bodyLarge: TextStyle(fontFamily: 'Vollkorn', fontSize: 18, height: 24 / 18, fontWeight: FontWeight.normal),
+    bodyMedium: TextStyle(fontFamily: 'Vollkorn', fontSize: 16, height: 20 / 16, fontWeight: FontWeight.normal),
+    bodySmall: TextStyle(fontFamily: 'Vollkorn', fontSize: 12, height: 16 / 12, fontWeight: FontWeight.normal),
+  );
+}
