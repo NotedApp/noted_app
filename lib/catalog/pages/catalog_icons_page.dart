58c791da88a1e8b185905ce037bad8872f080680
jaewon <jaewonmichael@gmail.com>
2023-05-18 23:04:17 -0400

    #24: implement catalog icons page

--- lib/catalog/pages/catalog_icons_page.dart
+++ lib/catalog/pages/catalog_icons_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:noted_app/widget/common/icon/noted_icons.dart';
 
 class CatalogIconsPage extends StatelessWidget {
   const CatalogIconsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return GridView.count(
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+      crossAxisCount: 6,
+      children: NotedIcons.iconList.map((icon) => _buildIconSquare(icon)).toList(),
+    );
+  }
+
+  Widget _buildIconSquare(IconData icon) {
+    return Center(child: Icon(icon));
   }
 }
