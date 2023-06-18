9bf6b3eef7f921b95d487f492ec709f4e68b194d
jaewon <jaewonmichael@gmail.com>
2023-06-18 12:07:25 -0700

    61: add util tests

---
+++ test/util/noted_strings_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:noted_app/util/noted_strings.dart';
+
+void main() {
+  group('Noted Strings', () {
+    test('string get handles all cases', () {
+      expect(NotedStrings.getString(NotedStringDomain.common, 'confirm'), 'confirm');
+      expect(NotedStrings.getString(NotedStringDomain.common, 'cancel'), 'cancel');
+      expect(NotedStrings.getString(NotedStringDomain.common, 'random'), 'unknown');
+
+      expect(NotedStrings.getString(NotedStringDomain.settings, 'colorTitle'), 'colors');
+    });
+  });
+}
