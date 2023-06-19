2d46c23a9cf4ef5ba1ea24c37d0f586e2d40c3b8
jaewon <jaewonmichael@gmail.com>
2023-06-19 12:22:29 -0700

    50: add filled icon button tests

--- test/widget/common/button/noted_icon_button_test.dart
+++ test/widget/common/button/noted_icon_button_test.dart
@@ -51,9 +51,64 @@ void main() {
       expect(mediumFinder, findsOneWidget);
       expect(largeFinder, findsOneWidget);
 
-      expect(tester.getSize(smallFinder), equals(const Size(36, 36)));
-      expect(tester.getSize(mediumFinder), equals(const Size(44, 44)));
-      expect(tester.getSize(largeFinder), equals(const Size(54, 54)));
+      expect(tester.getSize(smallFinder), equals(const Size.square(36)));
+      expect(tester.getSize(mediumFinder), equals(const Size.square(44)));
+      expect(tester.getSize(largeFinder), equals(const Size.square(54)));
+
+      await tester.tap(smallFinder);
+      await tester.tap(mediumFinder);
+      await tester.tap(largeFinder);
+
+      verify(onPressed()).called(3);
+    });
+
+    testWidgets('filled icon button functions as expected', (tester) async {
+      MockVoidCallback onPressed = MockVoidCallback();
+      const Key smallKey = Key('small');
+      const Key mediumKey = Key('medium');
+      const Key largeKey = Key('large');
+
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: Column(
+            children: [
+              NotedIconButton(
+                key: smallKey,
+                icon: NotedIcons.h1,
+                type: NotedIconButtonType.filled,
+                size: NotedWidgetSize.small,
+                onPressed: onPressed,
+              ),
+              NotedIconButton(
+                key: mediumKey,
+                icon: NotedIcons.h2,
+                type: NotedIconButtonType.filled,
+                onPressed: onPressed,
+              ),
+              NotedIconButton(
+                key: largeKey,
+                icon: NotedIcons.h3,
+                type: NotedIconButtonType.filled,
+                size: NotedWidgetSize.large,
+                onPressed: onPressed,
+              ),
+            ],
+          ),
+        ),
+      );
+
+      Finder smallFinder = find.byKey(smallKey);
+      Finder mediumFinder = find.byKey(mediumKey);
+      Finder largeFinder = find.byKey(largeKey);
+
+      expect(smallFinder, findsOneWidget);
+      expect(mediumFinder, findsOneWidget);
+      expect(largeFinder, findsOneWidget);
+
+      expect(tester.getSize(smallFinder), equals(const Size.square(44)));
+      expect(tester.getSize(mediumFinder), equals(const Size.square(54)));
+      expect(tester.getSize(largeFinder), equals(const Size.square(64)));
 
       await tester.tap(smallFinder);
       await tester.tap(mediumFinder);
