413f70cd7d42c7e6a2e1bff967f1ea13e12da719
Jaewon Yang <jaewonmichael@gmail.com>
2023-07-22 20:21:50 -0400

    #100: componentize toolbar content

--- lib/ui/common/rich_text/toolbar/noted_rich_text_toolbar.dart
+++ lib/ui/common/rich_text/toolbar/noted_rich_text_toolbar.dart
@@ -27,77 +27,90 @@ class NotedRichTextToolbar extends StatelessWidget {
           ),
         ],
       ),
-      child: Wrap(
-        alignment: WrapAlignment.spaceBetween,
-        spacing: 4,
-        runSpacing: 6,
-        children: [
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.bold,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.italic,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.underline,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.strikethrough,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.h1,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.h2,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.h3,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.ul,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.ol,
-            colors: colors,
-          ),
-          NotedRichTextToggleButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.taskList,
-            colors: colors,
-          ),
-          NotedRichTextColorButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.textColor,
-            colors: colors,
-          ),
-          NotedRichTextColorButton(
-            controller: controller,
-            attribute: NotedRichTextAttribute.textBackground,
-            colors: colors,
-          ),
-          NotedRichTextLinkButton(
-            controller: controller,
-            colors: colors,
-          ),
-        ],
-      ),
+      child: _ToolbarDefault(controller: controller),
+    );
+  }
+}
+
+class _ToolbarDefault extends StatelessWidget {
+  final NotedRichTextController controller;
+
+  const _ToolbarDefault({required this.controller, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme colors = Theme.of(context).colorScheme;
+
+    return Wrap(
+      alignment: WrapAlignment.spaceBetween,
+      spacing: 4,
+      runSpacing: 6,
+      children: [
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.bold,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.italic,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.underline,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.strikethrough,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.h1,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.h2,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.h3,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.ul,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.ol,
+          colors: colors,
+        ),
+        NotedRichTextToggleButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.taskList,
+          colors: colors,
+        ),
+        NotedRichTextColorButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.textColor,
+          colors: colors,
+        ),
+        NotedRichTextColorButton(
+          controller: controller,
+          attribute: NotedRichTextAttribute.textBackground,
+          colors: colors,
+        ),
+        NotedRichTextLinkButton(
+          controller: controller,
+          colors: colors,
+        ),
+      ],
     );
   }
 }
