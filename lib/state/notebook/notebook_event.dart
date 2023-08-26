8976b44970ab568dc1a47bc613b9479a9b4fde63
Jaewon Yang <jaewonmichael@gmail.com>
2023-08-26 01:30:51 -0400

    #119: add notebook update state and events

---
+++ lib/state/notebook/notebook_event.dart
@@ -0,0 +1,26 @@
+import 'package:noted_app/state/noted_bloc.dart';
+import 'package:noted_models/noted_models.dart';
+
+sealed class NotebookEvent extends NotedEvent implements TrackableEvent {
+  const NotebookEvent();
+}
+
+class NotebookLoadNotesEvent extends NotebookEvent {}
+
+class NotebookAddNoteEvent extends NotebookEvent {
+  final NotebookNote note;
+
+  const NotebookAddNoteEvent(this.note);
+}
+
+class NotebookUpdateNoteEvent extends NotebookEvent {
+  final NotebookNote note;
+
+  const NotebookUpdateNoteEvent(this.note);
+}
+
+class NotebookDeleteNoteEvent extends NotebookEvent {
+  final NotebookNote noteId;
+
+  const NotebookDeleteNoteEvent(this.noteId);
+}
