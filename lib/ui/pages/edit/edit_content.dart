83b0c045a77a9f11d08f6bcc674e02d251b8f507
jaewon <jaewonmichael@gmail.com>
2024-02-17 06:16:47 +0900

    #184: add edit document field and update cookbook edit

--- lib/ui/pages/edit/edit_content.dart
+++ lib/ui/pages/edit/edit_content.dart
@@ -27,7 +27,7 @@ class EditContent extends StatelessWidget {
 
         return switch (note.plugin) {
           NotedPlugin.notebook => NotebookEditContent(note: note, updateNote: updateNote),
-          NotedPlugin.cookbook => CookbookEditContent(note: note, updateNote: updateNote),
+          NotedPlugin.cookbook => const CookbookEditContent(),
           // TODO: Update this to climbing edit content.
           NotedPlugin.climbing => const Center(child: CircularProgressIndicator()),
         };
